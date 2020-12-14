4000a0746298b0a963a3fb6b7f35e519748a94a3
richardmarquez2001 <richard.marquez.2001@gmail.com>
2020-12-14 13:12:39 -0500

    fixed navbar link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ export default class Navbar extends Component{
                  <Link to = "/"><li className = "list-name">Richard Marquez</li></Link>
 
                  <div className="list-right">
-                    <a href = "mailto:richard.marquez.2001@gmail.com"><li className = "list-right__item">Resume</li></a>
+                    <a href = "https://drive.google.com/file/d/1LNPLZ66WfB_bT0QPk_TvW9sNtsTuO7Tb/view?usp=sharing"><li className = "list-right__item">Resume</li></a>
                     <Link to = "/about"><li className = "list-right__item">About</li></Link>
                     <Link to = "/projects"><li className = "list-right__item">Projects</li></Link>
                  </div>
